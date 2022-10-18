1a36ba4abae38b66c21cd8d4d04a0d7f4a58fc78
441460372 <113971397+441460372@users.noreply.github.com>
2022-10-18 10:31:04 +0300

    Create zayed.dart
    first lab

---
+++ zayed.dart
@@ -0,0 +1,18 @@
+void main() {
+  print('ZAYED!');
+  String nk = "Hello Class Students";
+  print(nk);
+  
+  bool x;
+  x = 1000 > 200;
+  print(x);
+  
+  String name = "";
+  if (name.length != 0) {
+    print("String not empty!");
+  } else {
+    print("string is empty!");
+  }
+ }
+      
+      
